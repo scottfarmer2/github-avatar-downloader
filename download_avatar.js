6a8ed2082f838cb31227dfa152168ac864255d2f
Scott Farmer <sfarmer664@gmail.com>
2017-01-16 23:13:20 +0000

    step3

--- download_avatar.js
+++ download_avatar.js
@@ -2,7 +2,11 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, function(err, result) {
+function getRepoContributors(repoOwner, repoName, cb) {
+
+}
+
+getRepoContributors("repoOwner", "repoName", function(err, result) {
   console.log('Errors:', err);
   console.log('results:', results);
 });
