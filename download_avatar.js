8f5e9d6dbd61299322e7e59135dcf7335fd2b411
Scott Farmer <sfarmer664@gmail.com>
2017-01-16 23:34:09 +0000

    step 5

--- download_avatar.js
+++ download_avatar.js
@@ -2,11 +2,20 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = "scottfarmer2"
+var GITHUB_TOKEN = "2c34a0788448c53c392905082925d281c5d77881"
+
 function getRepoContributors(repoOwner, repoName, cb) {
 
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+  console.log(requestURL);
 }
 
-getRepoContributors("repoOwner", "repoName", function(err, result) {
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
   console.log('Errors:', err);
   console.log('results:', results);
 });
+
