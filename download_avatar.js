5d1802a9023433b89c80e6f997250e0cfa9fe5f6
Scott Farmer <sfarmer664@gmail.com>
2017-01-17 01:27:33 +0000

    step 5

--- download_avatar.js
+++ download_avatar.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -6,16 +7,22 @@ var GITHUB_USER = "scottfarmer2"
 var GITHUB_TOKEN = "2c34a0788448c53c392905082925d281c5d77881"
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers:{
+      'User-Agent':'request'
+    }
+  }
 
-  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-
-  console.log(requestURL);
+  request(options, function(error, response, body) {
+    console.log(response.statusCode);
+    if (!error && response.statusCode == 200) {
+      cb(error, JSON.parse(body));
+    }
+  });
 }
-
-
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log('Errors:', err);
-  console.log('results:', results);
+  console.log('results:', result);
 });
-
